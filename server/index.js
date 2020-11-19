1806a0a9bd1ea99bf71ebe4a7594df6884f9e18a
devchea <rchea817@gmail.com>
2020-11-19 11:17:17 -0600

    "updates"

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const pool = require("./db")
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+//Routes
+
+//create a todo
+
+app.post("/todos", (req, res) => {
+  
+})
+
+//get all todos
+
+//get a todo
+
+//update a todo
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
