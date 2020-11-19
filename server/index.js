1c6cd6a9f25fb73316a99a80dd50b542a0662655
devchea <rchea817@gmail.com>
2020-11-19 12:14:52 -0600

    "updates"

--- server/index.js
+++ server/index.js
@@ -11,9 +11,14 @@ app.use(express.json())
 
 //create a todo
 
-app.post("/todos", (req, res) => {
+app.post("/todos", async(req, res) => {
   try {
     const { description } = req.body
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *", 
+      [description]
+    )
+    res.json(newTodo.rows[0])
   } catch (err) {
     console.error(err.message);
   }
@@ -21,9 +26,38 @@ app.post("/todos", (req, res) => {
 
 //get all todos
 
+app.get("/todos", async(req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo")
+    res.json(allTodos.rows)
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
 //get a todo
 
+app.get("/todos/:id", async(req, res) => {
+  try {
+    const { id } = req.params
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+    res.json(todo.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 //update a todo
+
+app.put("/todos/:id", async(req,res) => {
+  try {
+    const { id } = req.params
+    const { description } = req.body  
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//delete a todo
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
