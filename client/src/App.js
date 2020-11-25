8743b9e7d4a741ce3e9017fd004734a6234ed307
devchea <rchea817@gmail.com>
2020-11-25 16:56:41 -0600

    "updates"

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,6 @@ import InputTodo from './components/InputTodo'
 import ListTodo from './components/ListTodo'
 
 function App() {
-// const person = {
-// firstName: 'John',
-// lastName: 'Smith',
-// birthDate: '3/12/1950'
-// }
-//using the same person info we can apply destructuring
-// const { firstName, lastName, birthDate } = person
-//output below
-// const {
-//   firstName = "Richard",
-//   lastName = "Chea",
-//   birthDate = "5/11/1910",
-// } = person;
-
-// console.log('firstName:', firstName);
-// console.log('lastName:', lastName);
-// console.log('birthDate:', birthDate);
-
 
   return (
     <Fragment>
